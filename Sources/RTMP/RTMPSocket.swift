8da95fce13e708e9b9afb230523ef71dd7dcab19
leo150 <sokolov.lev95@gmail.com>
2019-09-06 15:41:48 -0700

    Finish write operation, close output stream, stop run loop

--- Sources/RTMP/RTMPSocket.swift
+++ Sources/RTMP/RTMPSocket.swift
@@ -88,14 +88,14 @@ final class RTMPSocket: NetSocket, RTMPSocketCompatible {
         super.initConnection()
     }
 
-    override func deinitConnection(isDisconnected: Bool) {
+    override func deinitConnection(isDisconnected: Bool, completion: @escaping (() -> Void) = {}) {
         if isDisconnected {
             let data: ASObject = (readyState == .handshakeDone) ?
                 RTMPConnection.Code.connectClosed.data("") : RTMPConnection.Code.connectFailed.data("")
             events.append(Event(type: .rtmpStatus, bubbles: false, data: data))
         }
         readyState = .closing
-        super.deinitConnection(isDisconnected: isDisconnected)
+        super.deinitConnection(isDisconnected: isDisconnected, completion: completion)
     }
 
     override func didTimeout() {
